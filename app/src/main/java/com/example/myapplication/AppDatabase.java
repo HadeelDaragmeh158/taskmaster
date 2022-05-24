98ebe1ec7d29c21729c41b7caafd84f40d179d99
HadeelDaragmeh158 <hadeelmohammad158@gmail.com>
2022-05-24 19:22:08 +0300

    Lab  29

---
+++ app/src/main/java/com/example/myapplication/AppDatabase.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Task.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+
+    private static AppDatabase appDatabase;
+
+
+    public AppDatabase (){}
+
+    public static AppDatabase getInstance(Context context){
+
+     if (appDatabase == null ) {
+         appDatabase = Room.databaseBuilder(context,
+                 AppDatabase.class,
+                 "AppDatabase").allowMainThreadQueries().build();
+     }
+        return appDatabase;
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private List<String> titles = new ArrayList<>();
+    private Task[] tasks = new Task[]{
+            new Task("Task1","basic of Android ","New"),
+            new Task("Task2","Data in task master ","Assigned"),
+            new Task("Task3","RecyclerViews for Displaying Lists of Data ","Complete"),
+            new Task("Task4","Room ","New")
+    };
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -21,12 +40,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         Button addTaskButton    = findViewById(R.id.addTaskButton);
         Button allTaskButton    = findViewById(R.id.allTaskButton);
         Button sittingButton    = findViewById(R.id.sittingButton);
-        Button buttonTask1      = findViewById(R.id.buttonTask1);
-        Button buttonTask2      = findViewById(R.id.buttonTask2);
-        Button buttonTask3      = findViewById(R.id.buttonTask3);
+//        Button buttonTask1      = findViewById(R.id.buttonTask1);
+//        Button buttonTask2      = findViewById(R.id.buttonTask2);
+//        Button buttonTask3      = findViewById(R.id.buttonTask3);
 
         addTaskButton.setOnClickListener(v -> {
 
@@ -53,37 +74,82 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+//
+//        buttonTask1.setOnClickListener(v->{
+//
+//            Log.i(TAG, "Button buttonTask1");
+//
+//            Intent taskDetailIntent = new Intent(this,TaskDetail.class);
+//            taskDetailIntent.putExtra("Title",buttonTask1.getText().toString());
+//            startActivity(taskDetailIntent);
+//
+//        });
+//
+//
+//        buttonTask2.setOnClickListener(v->{
+//
+//            Log.i(TAG, "Button buttonTask2");
+//
+//            Intent taskDetailIntent = new Intent(this,TaskDetail.class);
+//            taskDetailIntent.putExtra("Title",buttonTask2.getText().toString());
+//            startActivity(taskDetailIntent);
+//
+//        });
+//
+//
+//        buttonTask3.setOnClickListener(v->{
+//
+//            Log.i(TAG, "Button buttonTask3");
+//
+//            Intent taskDetailIntent = new Intent(this,TaskDetail.class);
+//            taskDetailIntent.putExtra("Title",buttonTask3.getText().toString());
+//            startActivity(taskDetailIntent);
+//
+//        });
 
-        buttonTask1.setOnClickListener(v->{
+        List<Task> tasks = AppDatabase.getInstance(getApplicationContext()).userDao().getAllTasks();
 
-            Log.i(TAG, "Button buttonTask1");
+        ListView ListTasks = findViewById(R.id.tasks);
+        ArrayAdapter<Task> taskArrayAdapter = new ArrayAdapter<Task>(
+                this,
+                android.R.layout.simple_list_item_2,
+                android.R.id.text2,
+                tasks
+        ) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                    View view = super.getView(position, convertView, parent);
+                    TextView title = (TextView) view.findViewById(android.R.id.text1);
+                    TextView body = (TextView) view.findViewById(android.R.id.text2);
 
-            Intent taskDetailIntent = new Intent(this,TaskDetail.class);
-            taskDetailIntent.putExtra("Title",buttonTask1.getText().toString());
-            startActivity(taskDetailIntent);
+                    title.setText(tasks.get(position).getTitle());
+                    body.setText(tasks.get(position).getBody());
 
-        });
+                    return view;
+            }
+        };
+//        tasksList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent taskIntent = new Intent(getApplicationContext(),TaskDetail.class);
+//                taskIntent.putExtra("title",tasks[i].getTitle());
+//                taskIntent.putExtra("body",tasks[i].getBody());
+//                taskIntent.putExtra("state",tasks[i].getState());
+//                startActivity(taskIntent);
+//            }
+//        });
+        ListTasks.setAdapter(taskArrayAdapter);
 
-
-        buttonTask2.setOnClickListener(v->{
-
-            Log.i(TAG, "Button buttonTask2");
-
-            Intent taskDetailIntent = new Intent(this,TaskDetail.class);
-            taskDetailIntent.putExtra("Title",buttonTask2.getText().toString());
-            startActivity(taskDetailIntent);
-
-        });
-
-
-        buttonTask3.setOnClickListener(v->{
-
-            Log.i(TAG, "Button buttonTask3");
-
-            Intent taskDetailIntent = new Intent(this,TaskDetail.class);
-            taskDetailIntent.putExtra("Title",buttonTask3.getText().toString());
-            startActivity(taskDetailIntent);
-
+        ListTasks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent taskIntent = new Intent(getApplicationContext(),TaskDetail.class);
+                taskIntent.putExtra("title",tasks.get(position).getTitle());
+                taskIntent.putExtra("body",tasks.get(position).getBody());
+                taskIntent.putExtra("state",tasks.get(position).getState());
+                startActivity(taskIntent);
+            }
         });
 
 
